d1f151b4ccf51ecce85c187608e071bd019f3c58
jmed0002 <jmed0002@student.monash.edu>
2025-03-30 09:37:08 +1100

    change base address

--- software/BasicInterrupts/MiniSystem.h
+++ software/BasicInterrupts/MiniSystem.h
@@ -2,11 +2,11 @@
 #define __SYSTEM_H_
 
 // KEY
-#define KEY_BASE 0x3010
+#define KEY_BASE 0x3030
 #define KEY_IRQ 0
 #define KEY_IRQ_INTERRUPT_CONTROLLER_ID 0
 // LEDR
-#define LEDR_BASE 0x3020
+#define LEDR_BASE 0x3010
 
 
 #endif /* __SYSTEM_H_ */
